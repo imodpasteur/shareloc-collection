95729485f96f2412f8fb8418dd20441b638ed378
Wei Ouyang <oeway007@gmail.com>
2022-07-12 03:02:30 -0700

    generate csv

--- scripts/generate-collection.py
+++ scripts/generate-collection.py
@@ -20,6 +20,8 @@ S3_DATA_DIR = "pointclouds"
 S3_KEY = os.environ.get("S3_KEY")
 S3_SECRET = os.environ.get("S3_SECRET")
 
+assert S3_ENDPOINT is not None, "S3_ENDPOINT must be set"
+
 S3_URL = f"{S3_ENDPOINT}/{S3_BUCKET}/{S3_DATA_DIR}"
 
 SUMMARY_FIELDS = [
@@ -43,55 +45,76 @@ SUMMARY_FIELDS = [
 ]
 
 
-def generate_potree(rdf, dataset_dir, force=False):
+def convert_formats(rdf, dataset_dir, force=False, potree=False, csv=False):
+    if not potree and not csv:
+        return
     attachments = rdf["attachments"]
     rdf_url = rdf["rdf_source"]
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=S3_ENDPOINT,
+        aws_access_key_id=S3_KEY,
+        aws_secret_access_key=S3_SECRET,
+    )
     for sample in attachments["samples"]:
         for file in sample.get("files", []):
             if file["name"].endswith(".smlm"):
-                # TODO: check if the file is already exists in the s3
-                zip_name = os.path.join(
-                    rdf["doi"],
-                    sample["name"],
-                    file["name"].replace(".smlm", ".potree.zip"),
-                )
-                target_url = S3_URL + "/" + zip_name
-                if not force:
-                    r = requests.head(target_url)
-                    if r.status_code == 200:
-                        continue
-                s3_client = boto3.client(
-                    "s3",
-                    endpoint_url=S3_ENDPOINT,
-                    aws_access_key_id=S3_KEY,
-                    aws_secret_access_key=S3_SECRET,
-                )
-                object_name = S3_DATA_DIR + "/" + zip_name
-
                 file_path = os.path.join(
                     dataset_dir, rdf["doi"], sample["name"], file["name"]
                 )
                 os.makedirs(
                     os.path.join(dataset_dir, rdf["doi"], sample["name"]), exist_ok=True
                 )
-                # download the file
-                download_url(
-                    resolve_url(rdf_url, sample["name"] + "/" + file["name"]),
-                    file_path,
-                )
-                print("Converting " + file_path + "...")
-                convert_potree(file_path, True)
-
-                potree_file_path = file_path.replace(".smlm", ".potree.zip")
-                print("Uploading " + potree_file_path + " to s3...")
-                s3_client.upload_file(potree_file_path, S3_BUCKET, object_name)
-
+                sample_path = os.path.join(rdf["doi"], sample["name"])
+                
+                response = s3_client.list_objects(Bucket=S3_BUCKET, Prefix=S3_DATA_DIR + "/" + os.path.join(sample_path) + "/")
+                existing_files = [f['Key'] for f in response['Contents']]
+                if potree:
+                    potree_files = sorted([f for f in existing_files if f.endswith(".potree.zip")])
+                    if not potree_files or force:
+                        if not os.path.exists(file_path):
+                            # download the file
+                            download_url(
+                                resolve_url(rdf_url, sample["name"] + "/" + file["name"]),
+                                file_path,
+                            )
+                        print("Converting " + file_path + " to potree...")
+                        files = convert_potree(file_path, True)
+                        potree_files = []
+                        for potree_file_path in files:
+                            object_name = S3_DATA_DIR + "/" + os.path.join(sample_path, os.path.basename(potree_file_path))
+                            print("Uploading " + potree_file_path + " to s3...")
+                            s3_client.upload_file(potree_file_path, S3_BUCKET, object_name)
+                            print("potree file uploaded successfully")
+                            os.remove(potree_file_path)
+                            potree_files.append(object_name)
+                    file['potree'] = potree_files
+                if csv:
+                    csv_files = sorted([f for f in existing_files if f.endswith(".csv")])
+                    if not csv_files or force:
+                        if not os.path.exists(file_path):
+                            # download the file
+                            download_url(
+                                resolve_url(rdf_url, sample["name"] + "/" + file["name"]),
+                                file_path,
+                            )
+                        print("Converting " + file_path + " to csv...")
+                        files = convert_smlm(file_path, delimiter=",", extension=".csv")
+                        csv_files = []
+                        for csv_file_path in files:
+                            object_name = S3_DATA_DIR + "/" + os.path.join(sample_path, os.path.basename(csv_file_path))
+                            print("Uploading " + csv_file_path + " to s3...")
+                            s3_client.upload_file(csv_file_path, S3_BUCKET, object_name)
+                            print("csv file uploaded successfully")
+                            os.remove(csv_file_path)
+                            csv_files.append(object_name)
+                    file['csv'] = csv_files
                 # Remove the folder
                 shutil.rmtree(os.path.join(dataset_dir, rdf["doi"]))
-                print("Potree file generated successfully: " + target_url)
+                
 
 
-def generate_collection(potree=False, force=False):
+def generate_collection(potree=False, csv=False, force=False):
     rdfs = []
     with open("collection.yaml", "rb") as f:
         collection = yaml.safe_load(f.read())
@@ -112,8 +135,8 @@ def generate_collection(potree=False, force=False):
             r.text.replace("!<tag:yaml.org,2002:js/undefined>", ""),
         )
         rdf.update(item)
-        if potree:
-            generate_potree(rdf, "datasets", force)
+        if potree or csv:
+            convert_formats(rdf, "datasets", force, potree, csv)
         summary = {k: v for k, v in rdf.items() if k in SUMMARY_FIELDS}
         rdfs.append(summary)
 
@@ -136,9 +159,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--potree', action='store_true',
                         help='Convert to potree and upload')
+    parser.add_argument('--csv', action='store_true',
+                        help='Convert to csv and upload')
     parser.add_argument('--force', action='store_true',
                         help='Force regenerate and upload')
 
     args = parser.parse_args()
     
-    generate_collection(potree=args.potree, force=args.force)
+    generate_collection(potree=args.potree, csv=args.csv, force=args.force)
